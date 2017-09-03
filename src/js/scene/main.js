067fa0d15cf48402c7f861f7948a43ed3cc68c60
sairoutine <sairoutine@gmail.com>
2017-09-03 21:48:21 +0900

    update

--- src/js/scene/main.js
+++ src/js/scene/main.js
@@ -7,14 +7,18 @@ var CONSTANT = require('../hakurei').constant;
 
 var UIParts = require('../hakurei').object.ui_parts;
 
+// スコア閾値ゲージの最大値
+var GAUGE_MAX = 100;
+
+
 /* TODO:
-クリックするとメリーが落ちてくる
-クリックするとスコアが上がる
-スコア閾値ゲージ作成
 スコアが閾値を超えると、スーパー蓮子ちゃんタイムになる
 → 背景変更／ロゴがにゅいんと出て来る
 クリックすると蓮子が回転する
 → 大小／回転／表情変更
+
+蓮子をメリーの前へ
+画面外オブジェクトの削除
 */
 
 var SceneStage = function(core) {
@@ -65,7 +69,7 @@ SceneStage.prototype.beforeDraw = function(){
 		if(this.renko.checkCollisionWithPosition(x, y)) {
 			if (!this.is_renko_chan_time) {
 				// メリー生成
-				//this.addObject(this._createMerry());
+				this.addObject(this._createMerry());
 
 				this.score += 1;
 				this._renko_chan_score += 1;
@@ -89,16 +93,24 @@ SceneStage.prototype.draw = function(){
 
 
 	// 背景描画
-	var bg = this.core.image_loader.getImage("bg_super");
-	ctx.drawImage(bg,
-					0,
-					0,
-					bg.width,
-					bg.height,
-					0,
-					0,
-					this.core.width,
-					this.core.height);
+	if (!this.is_renko_chan_time) {
+		ctx.fillStyle = "white";
+		ctx.fillRect(0, 0, this.width, this.height);
+
+	}
+	else {
+		// スーパー蓮子ちゃんタイム
+		var bg = this.core.image_loader.getImage("bg_super");
+		ctx.drawImage(bg,
+						0,
+						0,
+						bg.width,
+						bg.height,
+						0,
+						0,
+						this.core.width,
+						this.core.height);
+	}
 
 	base_scene.prototype.draw.apply(this, arguments);
 
@@ -108,6 +120,9 @@ SceneStage.prototype.draw = function(){
 	ctx.textBaseAlign = 'middle';
 	ctx.fillStyle = 'rgb( 0, 0, 0 )';
 	ctx.fillText("Score: " + this.score, this.width - 10, this.height - 10);
+
+	// スコア閾値ゲージ表示
+	this._showGauge();
 
 	ctx.restore();
 };
@@ -154,6 +169,85 @@ SceneStage.prototype._createRenko = function(){
 
 	return canvas;
 };
+
+SceneStage.prototype._createMerry = function(){
+	// create canvas
+	var canvas = window.document.createElement('canvas');
+	canvas.width  = 400;
+	canvas.height = 320;
+	var ctx2 = canvas.getContext('2d');
+
+	var merry_base = this.core.image_loader.getImage("merry_base");
+	ctx2.drawImage(merry_base,
+					0,
+					0,
+					merry_base.width,
+					merry_base.height
+	);
+
+	var merry_eye = this.core.image_loader.getImage("merry_eye_00");
+	ctx2.drawImage(merry_eye,
+					0,
+					0,
+					merry_eye.width,
+					merry_eye.height
+	);
+
+	var merry_eyebrow = this.core.image_loader.getImage("merry_eyebrow_00");
+	ctx2.drawImage(merry_eyebrow,
+					0,
+					0,
+					merry_eyebrow.width,
+					merry_eyebrow.height
+	);
+
+	var merry_mouse = this.core.image_loader.getImage("merry_mouse_00");
+	ctx2.drawImage(merry_mouse,
+					0,
+					0,
+					merry_mouse.width,
+					merry_mouse.height
+	);
+
+	// 出現位置(ランダム)
+	var x = Math.floor(Math.random() * this.width);
+	var y = -60;
+	// メリーの角度
+	var rotate = Math.floor(Math.random() * 360);
+	var speed = 10 + Math.floor(Math.random() * 5);
+
+	return new UIParts(this, x, y, 0, 0, function draw () {
+		this.y(this.y() + speed);
+
+		var ctx = this.core.ctx;
+		ctx.save();
+		ctx.translate(this.x(), this.y());
+		ctx.rotate(rotate);
+		ctx.drawImage(canvas, -canvas.width/2, -canvas.height/2, canvas.width*0.5, canvas.height*0.5);
+
+		ctx.restore();
+	});
+
+
+};
+
+var VITAL_OUTLINE_MARGIN = 5;
+// スコア閾値ゲージ
+SceneStage.prototype._showGauge = function(){
+	var ctx = this.core.ctx;
+	ctx.save();
+
+	ctx.fillStyle = 'red';
+	ctx.fillRect(
+		VITAL_OUTLINE_MARGIN,
+		VITAL_OUTLINE_MARGIN,
+		this._renko_chan_score / GAUGE_MAX * (this.width - VITAL_OUTLINE_MARGIN * 2),
+		VITAL_OUTLINE_MARGIN * 4
+	);
+
+	ctx.restore();
+};
+
 
 
 
